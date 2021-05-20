19238986a6b27dea5fafa4d00016303c300064b2
hugojesusmr <hugojesusmr@gmail.com>
2021-05-19 22:00:42 -0500

    updating

--- gateway-service/src/main/java/com/gateway/servicegateway/ServiceGatewayApplication.java
+++ gateway-service/src/main/java/com/gateway/servicegateway/ServiceGatewayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ServiceGatewayApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceGatewayApplication.class, args);
 	}
